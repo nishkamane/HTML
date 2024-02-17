0e64cf72a51a7cb8398601177490d0711243d57e
nishkamane <108368593+nishkamane@users.noreply.github.com>
2024-02-17 19:07:47 +0530

    Add files via upload

---
+++ React/Task-3/src/App.tsx
@@ -0,0 +1,11 @@
+import Card from "./components/card/Card";
+import Navbar from "./components/nav-bar/Navbar";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Card />
+    </div>
+  );
+}
